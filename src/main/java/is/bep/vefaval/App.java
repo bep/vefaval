e7abc01c6e35e195765821f6d3a5ff5b9007be8a
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2015-10-24 00:23:19 +0200

    Add temporary property hack

--- src/main/java/is/bep/vefaval/App.java
+++ src/main/java/is/bep/vefaval/App.java
@@ -1,23 +1,18 @@
 package is.bep.vefaval;
 
-import no.difi.vefa.validator.Validator;
-import no.difi.vefa.validator.ValidatorBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Main entry application and config.
  */
 @SpringBootApplication
+@PropertySource("classpath:validator.properties")
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    @Bean
-    Validator validator() throws Exception {
-        return ValidatorBuilder.newValidator().build();
-    }
 }
